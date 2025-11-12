7c0e7f4d2b3e83434c1b3c66e30b981f61f9cfa6
Tom Mårdh <tom.mardh@gmail.com>
2025-11-12 23:50:07 +0100

    Scope360° User App Version 2025.11.10 available

--- common-content-script-service/content-script-service.d.ts
+++ common-content-script-service/content-script-service.d.ts
@@ -1,4 +1,1 @@
-declare function isInBrowserExtensionContext(): boolean;
-declare function hasChromeRuntimeId(): boolean;
-declare function hasBrowserRuntimeId(): boolean;
-declare function initializeContentScriptService(): void;
+export {};
